df51c789d970b7f11cfcd387b49706da0efad820
lys7375 <yushuolu7375@gmail.com>
2022-12-03 06:26:14 -0800

    14,15 done

--- Assignment 2/Assets/Scripts/CameraShaderController.cs
+++ Assignment 2/Assets/Scripts/CameraShaderController.cs
@@ -6,12 +6,15 @@ public class CameraShaderController : MonoBehaviour
 {
     private FullScreenShader fullScreenShader;
     private FogScreenShader fogScreenShader;
+    [SerializeField] private AudioSource dayBgm;
+    [SerializeField] private AudioSource nighBgm;
 
     // Start is called before the first frame update
     void Start()
     {
         fullScreenShader = this.GetComponent<FullScreenShader>();
         fogScreenShader = this.GetComponent<FogScreenShader>();
+        dayBgm.Play();
     }
 
     // Update is called once per frame
@@ -20,11 +23,33 @@ public class CameraShaderController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.U))
         {
             fullScreenShader.enabled = !fullScreenShader.enabled;
+            
+            if(fullScreenShader.enabled)
+            {
+                dayBgm.Stop();
+                nighBgm.Play();
+            }
+            else
+            {
+                nighBgm.Stop();
+                dayBgm.Play();
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.I))
         {
             fogScreenShader.enabled = !fogScreenShader.enabled;
+
+            if(fogScreenShader.enabled)
+            {
+                dayBgm.volume = 0.5f;
+                nighBgm.volume = 0.5f;
+            }
+            else
+            {
+                dayBgm.volume = 1f;
+                nighBgm.volume = 1f;
+            }
         }
     }
 }
